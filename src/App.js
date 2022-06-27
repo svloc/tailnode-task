7104649cdb327baa152ceee3ecb4533d015cbeb2
svloc <svloc39@gmail.com>
2022-06-27 15:31:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ export default function App() {
   const[selectVal,setSelectVal]=useState('todo');
   const[title,setTitle]=useState('');
   const[data,setData]=useState('');
+  const[comData,setComData]=useState('');
   const[add,setAdd]=useState(true);
   const[current,setCurrent]=useState(false);
   const[completed,setCompleted]=useState(false);
-
-
   useEffect(()=>{
     getValue();
+    getValue_1();
   },[])
-
   const handleChangeInSelect=(e)=>{
     setSelectVal(e.target.value);
   }
@@ -29,9 +28,12 @@ export default function App() {
     localStorage.setItem("todo_data",JSON.stringify(todos));
     setTitle('');
     setSelectVal('todo');
+    getValue();
+    setAdd(false);
+    setCurrent(true);
+    setCompleted(false);
   }
-
-  function sidebarStatus(str){
+ const sidebarStatus=(str)=>{
    switch(str){
     case 'add':
       setAdd(true);
@@ -50,20 +52,49 @@ export default function App() {
       break;  
    }
   }
-  const getValue=()=>{
+ const getValue=()=>{
     setData(JSON.parse(localStorage.getItem('todo_data')));
   }
-  const handleSatus=(i)=>{
-   data.slice(i+1);
+ const getValue_1=()=>{
+    setComData(JSON.parse(localStorage.getItem('ed')));
   }
- 
+ const handleSatus=(i)=>{
+   data[i].status='completed';
+   localStorage.setItem('todo_data',JSON.stringify(data));
+   getValue();
+   let ed_data=[];
+   let todos = localStorage.getItem('ed');
+
+   for(let i=0;i<data.length;i++){
+   if(data[i].status=='completed'){
+    todos = todos ? JSON.parse(todos) : [];
+    todos.push(data[i]);
+   }
+   else{
+    ed_data.push(data[i]);
+   }
+  }
+  localStorage.setItem('todo_data',JSON.stringify(ed_data));
+  getValue();
+  localStorage.setItem('ed',JSON.stringify(todos));
+  getValue_1();
+  setAdd(false);
+  setCurrent(false);
+  setCompleted(true);
+  }
+ const onClear=()=>{
+    localStorage.clear();
+    getValue();
+    getValue_1();
+  }
+
   return (
     <div className="container">
-     
-     {/* sidebar start */}
+    {/* sidebar start */}
      <div className="sidebar">
       <nav className="nav">
         <ul className="nav-ul">
+          <li className="nav-btn"><button onClick={onClear} className='btn btn-black'>Delete All Todos</button></li>
           <li className={add? 'active nav-li':'nav-li'} onClick={()=>sidebarStatus('add')}>Add Todo +</li>
           <li className={current? 'active nav-li':'nav-li'} onClick={()=>sidebarStatus('current')}>Current Todo</li>
           <li className={completed? 'active nav-li':'nav-li'} onClick={()=>sidebarStatus('completed')}>Completed Todo </li>
@@ -71,12 +102,10 @@ export default function App() {
       </nav>
      </div>
      {/* sidebar end */}
-
      {/* add Todo start */}
      {add && !current && !completed &&
      <div className="add-todo" >
-    
-      <form className="form-todo" onSubmit={handleSubmit}>
+     <form className="form-todo" onSubmit={handleSubmit}>
         <div>Enter Todo Title</div> 
         <input className='form-input' type="text" name='title' onChange={handleChangeInInput} value={title}/>
         <div>Select Status</div>
@@ -88,12 +117,9 @@ export default function App() {
         <button className="btn btn-black f-right" type="submit" >Add Todo</button>
       </form>
      </div>}
-    
-     {/* add Todo end */}
-     
-     {/* current todo start */}
+    {/* add Todo end */}
+    {/* current todo start */}
      {!add && current && !completed &&
-     
      <div className="current-todo">
       <table>
       <thead>
@@ -104,20 +130,19 @@ export default function App() {
         </tr>
       </thead>
       <tbody>
-        {data.map((val,i)=>{
+        {data ||data!=null ? data.map((val,i)=>{
           return(
         <tr key={i}>
           <td>{val.title}</td>
           <td>{val.status}</td>
           <td><button className="btn btn-black btn-sm" onClick={()=>handleSatus(i)} >Mark as Complete</button></td>
         </tr>)
-        })}
+        }):<tr>No Data Available</tr>}
       </tbody>  
      </table>
      </div>
      }
      {/* current todo end */}
-
      {/* completed todo start */}
      {!add && !current && completed &&
      <div className="completed-todo">
@@ -126,22 +151,23 @@ export default function App() {
         <tr>
           <th>Todo</th>
           <th>Status</th>
-          <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
+        {comData ||comData!=null ?
+        comData.map((val,i)=>{
+          return(
+        <tr key={i}>
+          <td>{val.title}</td>
+          <td>{val.status}</td>
         </tr>
+        )}):
+        <tr>No Data Available</tr>}
       </tbody>
      </table>
      </div>
      }
      {/* completed todo end */}
-
-
     </div>
   );
 }
